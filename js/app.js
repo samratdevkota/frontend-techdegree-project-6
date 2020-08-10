506b48f19c48801ddb736496897b749bff9710ff
Samrat Devkota <samratdevkota1@gmail.com>
2020-08-10 18:02:07 +0545

    reate getRandomPhraseAsArray function
    - that randomly choose a phrase from the phrases array and split that phrase into a new array of characters.
    - The function should then return the new character array.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-
 const startGameBtn = document.querySelector('.btn__reset');
 let missed = 0;
+
+const phrases = [
+    "A blessing in disguise",
+    "A dime a dozen",
+    "Beat around the bush",
+    "Better late than never",
+    "A Piece of Cake"
+];
+
+function getRandomPhraseAsArray(arr){
+    const randomNumber = Math.floor(Math.random() * (arr.length));
+    const randomPhrase = arr[randomNumber];
+    const characterArray = randomPhrase.split(" ");
+    return characterArray;
+}
+
 startGameBtn.addEventListener('click',(e)=>{
     e.target.parentNode.style.visibility = "hidden";
 });
